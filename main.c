b25a6caca8e4cc905c19519f849554c4daf70756
Killian Schneider <80042061+yoschigamer@users.noreply.github.com>
2022-09-13 13:37:32 +0200

    Update main.c

--- main.c
+++ main.c
@@ -217,4 +217,21 @@ int ft_fibonacci(int index)
     return nb;
 }
 -----------------------------------------------------------------------------
+
+------------------------------ CALCUL RACINE CARRÉ --------------------------
+
+int ft_sqrt(int nb)
+{
+    int i = 0;
+    if (nb == 0)
+        return 0;
+    while (i * i < nb)
+    {
+        i++;
+    }
+    if (i * i == nb)
+        return i;
+    else 
+        return 0;
+}
 */
